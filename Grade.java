555258589e9a2431803643220631e0a651be25af
RaphaDevsearch <mizarapahelpro@gmail.com>
2025-06-07 12:57:57 +0530

    add prototype for grade

--- Grade.java
+++ Grade.java
@@ -1,3 +1,21 @@
 public class Grade {
-    
+    private Course course;
+    private double score;
+
+    public Grade(Course course, double score) {
+        this.course = course;
+        this.score = score;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public double getScore() {
+        return score;
+    }
+    @Override
+    public String toString(){
+        return course.getCourseName() + " - " + score;
+    }
 }
