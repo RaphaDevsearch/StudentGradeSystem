f055b3b7756b8668922b6432033f57c399aa01d0
RaphaDevsearch <mizarapahelpro@gmail.com>
2025-06-07 13:05:46 +0530

    add prototype for student

--- Student.java
+++ Student.java
@@ -1,3 +1,39 @@
+import java.util.ArrayList;
+
 public class Student {
-    
+    private String id;
+    private String name;
+    private ArrayList<Grade> grades;
+
+    public Student(String id, String name) {
+        this.id = id;
+        this.name = name;
+        this.grades = new ArrayList<>();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Grade> getGrades() {
+        return grades;
+    }
+
+    public double calculateAverage() {
+        if (grades.isEmpty()) return 0;
+        double sum = 0;
+        for(Grade g : grades){
+            sum += g.getScore();
+        }
+        return sum/grades.size();
+    }
+
+    @Override
+    public String toString(){
+        return id + " - " + name;
+    }
 }
