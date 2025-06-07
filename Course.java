d4fa377965929d3a327c534529296b9619f4180b
RaphaDevsearch <mizarapahelpro@gmail.com>
2025-06-07 12:53:24 +0530

    create protorype for Course

--- Course.java
+++ Course.java
@@ -1,3 +1,22 @@
 public class Course {
+    private String courseId;
+    private String courseName;
+
+    public Course(String id, String name) {
+        this.courseId = id;
+        this.courseName = name;
+    }
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
     
+    @Override
+    public String toString(){
+        return courseId + " - " + courseName;
+    }
 }
